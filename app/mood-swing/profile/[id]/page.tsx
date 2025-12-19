bf5c79386f1b4a0833009e874395ddeb6febc917
khoapsn <khoa.psn@gmail.com>
2025-12-19 18:16:58 +0700

    dev sieu nhieu thu

--- app/mood-swing/profile/[id]/page.tsx
+++ app/mood-swing/profile/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useToast } from "@/app/_libs/contexts";
 import { LinearProgress } from "@mui/material";
-import dayjs from "dayjs";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { ProfileContext } from "../../_libs/contexts";
@@ -10,12 +9,9 @@ import { getProfile } from "../../_libs/data";
 import { Profile } from "../../_libs/models";
 import CardMoods from "./card-moods";
 
-const today = dayjs();
-
 export default function Page() {
     const { id } = useParams<{ id: string }>();
     const [profile, setProfile] = useState<Profile>();
-    const [moods, setMoods] = useState<string[]>((new Array(dayjs(today).daysInMonth())).fill('happy'));
     const toast = useToast();
 
     const refresh = async () => {
