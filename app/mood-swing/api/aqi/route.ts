e967f9e056e74a45ed14fe5d86be813cef09a219
khoapsn <khoa.psn@gmail.com>
2026-01-05 17:57:02 +0700

    minor

---
+++ app/mood-swing/api/aqi/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export const url = 'http://api.airvisual.com/v2/city?city=Hanoi&state=Hanoi&country=Vietnam&key=82350757-0540-49aa-82c9-48bf1594612e';
+
+export async function GET() {
+    const data = await fetch(url);
+    const json = await data.json();
+    const value = json.data.current.pollution.aqius;
+    const content = `value\r\n${value}`;
+
+    return new NextResponse(content, {
+        headers: {
+            'Content-Type': 'application/csv',
+            'Content-Disposition': 'attachment; filename="aqi.csv"'
+        },
+    });
+}
